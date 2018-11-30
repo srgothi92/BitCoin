1b9bc3fa33b45bced15589584090afa1e0a8b0f2
Shailesh <shaileshgothiece@gmail.com>
2018-11-30 18:10:37 -0500

    added code to add new block to the chain

--- lib/BlockChain/Block.ex
+++ lib/BlockChain/Block.ex
@@ -37,19 +37,19 @@ defmodule BITCOIN.BlockChain.Block do
       Integer.to_string(block.nonce) <> Integer.to_string(block.index)
   end
 
-  defp hash(%__MODULE__{} = block) do
+  def hash(%__MODULE__{} = block) do
     headerString = stringifyHeader(block)
     :crypto.hash(:sha256, :crypto.hash(:sha256, headerString))
   end
 
-  def createBlock(previousHash, nonce, transactions) do
+  def createBlock(previousHash, transactions) do
     %__MODULE__{
       previous_block: previousHash,
       timestamp: :os.system_time(:millisecond),
-      nonce: nonce,
+      nonce: 0,
       transactions: transactions,
       index: 0,
-      hash: "0012345"
+      hash: "0"
     }
   end
 end
