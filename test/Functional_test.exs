282dcc90e6a3564d24cf60ccb3343f716b87d084
Shailesh <shaileshgothiece@gmail.com>
2018-12-02 06:53:02 -0500

    Trying to add Transaction verify test cases by not working

--- test/Functional_test.exs
+++ test/Functional_test.exs
@@ -1,7 +1,6 @@
 defmodule BITCOIN.BlockChain.Functional_test do
-use ExUnit.Case, async: true
-import MockChain
-alias BITCOIN.Wallet.Wallet
+  import MockChain
+  alias BITCOIN.Wallet.Wallet
   alias BITCOIN.BlockChain.{Chain, TransactionQueue, TxOutput, Transaction}
   alias BITCOIN.UserNode
 
@@ -12,24 +11,28 @@ alias BITCOIN.Wallet.Wallet
   end
 
   test "Su balance", %{nodeSu: nodeSu, nodeMu: nodeMu, nodeLu: nodeLu} do
-    suBalance =  GenServer.call(elem(nodeSu,1),:balance)
+    suBalance = GenServer.call(elem(nodeSu, 1), :balance)
     assert suBalance == 36
   end
 
   test "Mu Balance", %{nodeSu: nodeSu, nodeMu: nodeMu, nodeLu: nodeLu} do
-    muBalance =  GenServer.call(elem(nodeMu,1),:balance)
+    muBalance = GenServer.call(elem(nodeMu, 1), :balance)
     assert muBalance == 28
   end
 
   test "Lu Balance", %{nodeSu: nodeSu, nodeMu: nodeMu, nodeLu: nodeLu} do
-    luBalance =  GenServer.call(elem(nodeLu,1),:balance)
+    luBalance = GenServer.call(elem(nodeLu, 1), :balance)
     assert luBalance == 6
   end
 
-  test "Su trnasfer 10 to Mu", %{nodeSu: nodeSu, nodeMu: nodeMu}do
-    GenServer.call(elem(nodeSu,1),{:send, 6,  GenServer.call(elem(nodeMu,1), :getWallet).address})
-    suBalance =  GenServer.call(elem(nodeSu,1),:balance)
-    muBalance =  GenServer.call(elem(nodeMu,1),:balance)
+  test "Su trnasfer 10 to Mu", %{nodeSu: nodeSu, nodeMu: nodeMu} do
+    GenServer.call(
+      elem(nodeSu, 1),
+      {:send, 6, GenServer.call(elem(nodeMu, 1), :getWallet).address}
+    )
+
+    suBalance = GenServer.call(elem(nodeSu, 1), :balance)
+    muBalance = GenServer.call(elem(nodeMu, 1), :balance)
     assert muBalance == 34
     assert suBalance == 30
   end
