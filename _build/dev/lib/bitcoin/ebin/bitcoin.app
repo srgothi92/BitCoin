b6dc551c6588610e389c835b711d118fb06dbf38
Sruthi Korukonda <Sruthi@Sriharshas-MacBook-Air.local>
2018-12-12 18:35:27 -0500

    changed bar chart but x-axis labels not showing

---
+++ _build/dev/lib/bitcoin/ebin/bitcoin.app
@@ -0,0 +1,6 @@
+{application,bitcoin,
+             [{applications,[kernel,stdlib,elixir,logger]},
+              {description,"bitcoin"},
+              {modules,[]},
+              {registered,[]},
+              {vsn,"0.1.0"}]}.
--- bitcoin/assets/js/dashboard.js
+++ bitcoin/assets/js/dashboard.js
@@ -4,34 +4,4 @@
     columns: 12
   });
 
-$(function() {
-  var ctx1 = document.getElementById("barChart");
-  var myChart1 = new Chart(ctx1, {
-    type: 'bar',
-    data: {
-      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
-      datasets: [
-        {
-          label: "Population (millions)",
-          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-          data: [2478,5267,734,784,433]
-        }
-      ]
-    },
-    options: {
-      legend: { display: false },
-      title: {
-        display: true,
-        text: 'Predicted world population (millions) in 2050'
-      },
-    pan: {
-      enabled: true,
-      mode: 'xy'
-  },
-  zoom: {
-      enabled: true,
-      mode: 'xy',
-  }
-}
-  })
-});
+
